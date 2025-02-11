18ec2d1225e2eca3ce8c4c281a4c61dcc3789736
Cozma Rares <cozmarares11@gmail.com>
2025-02-11 20:05:09 +0200

    feat: add tokens

---
+++ src/tokens.rs
@@ -0,0 +1,62 @@
+pub enum Token{
+    // literals
+    Number(f64),
+    Char(char),
+    String(String),
+    Identifier(String),
+
+    // constants
+    Null,
+    True,
+    False,
+
+    // keywords
+    Let,
+    If,
+    Then,
+    Else,
+    End,
+    For,
+    Execute,
+    While,
+    Do,
+    Until,
+    Print,
+    Read,
+    Throw,
+    Try,
+    Catch,
+    Function,
+    Return,
+    Continue,
+    Break,
+    Include,
+    Run,
+
+    // operators
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Modulo,
+    Equals,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Different,
+    Assignment,
+    Or,
+    And,
+
+    // delimiters
+    ParenLeft,
+    ParenRight,
+    BracketLeft,
+    BracketRight,
+    CurlyLeft,
+    CurlyRight,
+    Comma,
+    Colon,
+    Endline,
+}
