0b4a53a27c05f8db2e05d8de769aaf1c66dcfa12
Cozma Rares <cozmarares11@gmail.com>
2025-02-14 16:10:40 +0200

    feat: implement tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-mod tokens;
 mod lexer;
+mod tokens;
 
 use std::io;
 
 use anyhow::Result;
+use lexer::Lexer;
 
 fn main() -> Result<()> {
     let stdin = io::stdin();
     for line in stdin.lines() {
         let line = line?;
         println!("Read: {}", line);
+        println!("Lexed: {:?}", Lexer::tokenize(&line));
     }
     Ok(())
 }
